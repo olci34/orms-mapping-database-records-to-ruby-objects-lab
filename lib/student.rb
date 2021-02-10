890254d0cf9631325ee6f9653b5ad82515cbb799
github username <github email address>
2021-02-10 17:16:38 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,64 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+      new_student = self.new
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
+      new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    all_students = DB[:conn].execute(sql).map {|row| self.find_by_name(row[1])}
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 9
+    SQL
+    all_in_9 = DB[:conn].execute(sql).map {|row| self.find_by_name(row[1])}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade < 12
+    SQL
+    all_but_12th = DB[:conn].execute(sql).map {|row| self.find_by_name(row[1])}
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+    first_X_students = DB[:conn].execute(sql, x).map {|row| self.find_by_name(row[1])}
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10 LIMIT 1
+    SQL
+    first_X_student = DB[:conn].execute(sql).map {|row| self.find_by_name(row[1])}[0]
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = ?
+    SQL
+    all_X_students = DB[:conn].execute(sql, x).map {|row| self.find_by_name(row[1])}
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+    student = DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}[0]
   end
   
   def save
